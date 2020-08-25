16d6089267e7215ab5bffc9f0fc8b98a5e40a40d
Eposey-creator <66450247+Eposey-creator@users.noreply.github.com>
2020-08-25 18:44:55 -0500

    Add files via upload

---
+++ FSDI 109 PROJECT/IONIC5-Angular9/src/polyfills.ts
@@ -0,0 +1,13 @@
+
+import './zone-flags';
+
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
